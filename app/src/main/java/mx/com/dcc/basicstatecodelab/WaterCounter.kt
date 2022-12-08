5f8b2e1ba1d090472f655ca52ed0a07c0dd9246d
Daniel Cinto <dansolo.cc@gmail.com>
2022-12-08 11:32:24 -0600

    Add state

--- app/src/main/java/mx/com/dcc/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/mx/com/dcc/basicstatecodelab/WaterCounter.kt
@@ -1,11 +1,12 @@
 package mx.com.dcc.basicstatecodelab
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,8 +14,8 @@ import mx.com.dcc.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
     Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
         Text(text = "You've $count glasses")
         Button(onClick = { count++ }, modifier = modifier.padding(top = 8.dp)) {
             Text(text = "Add one")
