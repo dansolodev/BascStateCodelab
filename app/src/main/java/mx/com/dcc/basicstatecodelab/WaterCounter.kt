9db01ab88c1579828ca378c4a50083ad0fcdfc77
Daniel Cinto <dansolo.cc@gmail.com>
2022-12-08 11:26:15 -0600

    Add button

--- app/src/main/java/mx/com/dcc/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/mx/com/dcc/basicstatecodelab/WaterCounter.kt
@@ -1,16 +1,35 @@
 package mx.com.dcc.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import mx.com.dcc.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've $count glasses",
-        modifier = modifier.padding(16.dp)
-    )
+    var count = 0
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(text = "You've $count glasses")
+        Button(onClick = { count++ }, modifier = modifier.padding(top = 8.dp)) {
+            Text(text = "Add one")
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    name = "WaterCounter",
+    heightDp = 320
+)
+@Composable
+fun WaterCounterPreview() {
+    BasicStateCodelabTheme {
+        WaterCounter(modifier = Modifier.fillMaxWidth())
+    }
 }
