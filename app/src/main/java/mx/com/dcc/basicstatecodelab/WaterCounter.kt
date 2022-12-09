863dd9e5bd9fca18b2b4038eb2e7652abff2c8af
Daniel Cinto <dansolo.cc@gmail.com>
2022-12-08 19:18:02 -0600

    State hoisting

--- app/src/main/java/mx/com/dcc/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/mx/com/dcc/basicstatecodelab/WaterCounter.kt
@@ -1,12 +1,12 @@
 package mx.com.dcc.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,5 +34,31 @@ fun WaterCounter(modifier: Modifier = Modifier) {
 fun WaterCounterPreview() {
     BasicStateCodelabTheme {
         WaterCounter(modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count = count, onIncrement = { count++ }, modifier)
+}
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(text = "You've had $count glasses")
+        }
+        Button(
+            onClick = onIncrement,
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text(text = "Add One")
+        }
     }
 }
