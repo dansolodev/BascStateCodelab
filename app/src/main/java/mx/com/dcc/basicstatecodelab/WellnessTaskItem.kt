61acc63269f2c3e9f5c1c98893f76cfb442cc416
Daniel Cinto <dansolo.cc@gmail.com>
2022-12-08 19:38:30 -0600

    List

--- app/src/main/java/mx/com/dcc/basicstatecodelab/WellnessTaskItem.kt
+++ app/src/main/java/mx/com/dcc/basicstatecodelab/WellnessTaskItem.kt
@@ -2,19 +2,26 @@ package mx.com.dcc.basicstatecodelab
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -24,11 +31,16 @@ fun WellnessTaskItem(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = taskName, modifier = Modifier
+            text = taskName,
+            modifier = Modifier
                 .weight(1f)
                 .padding(start = 16.dp)
         )
-        IconButton(onClick = { onClose() }) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+        IconButton(onClick = onClose) {
             Icon(
                 imageVector = Icons.Filled.Close,
                 contentDescription = "Close"
@@ -36,4 +48,16 @@ fun WellnessTaskItem(
         }
     }
 
+}
+
+@Composable
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { checkedState = !checkedState }, // onCheckedChange = { newValue -> checkedState = newValue }
+        onClose = { },
+        modifier = modifier
+    )
 }
---
+++ app/src/main/java/mx/com/dcc/basicstatecodelab/model/WellnessTask.kt
@@ -0,0 +1,6 @@
+package mx.com.dcc.basicstatecodelab.model
+
+data class WellnessTask(
+    val id: Int,
+    val label: String
+)
